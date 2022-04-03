ca0cff02faf3d3a64a593b8c7e9bd23eebe913ff
Yuukin256 <Yuukin256@gmail.com>
2022-04-04 00:40:45 +0900

    中央寄せ、右寄せのテーブルセルのコンポーネントを作成

---
+++ src/components/atoms/TableCellCenter.tsx
@@ -0,0 +1,8 @@
+import TableCell from '@mui/material/TableCell';
+import { styled } from '@mui/system';
+
+const TableCellCenter = styled(TableCell)({
+  textAlign: 'center',
+});
+
+export default TableCellCenter;
---
+++ src/components/atoms/TableCellRight.tsx
@@ -0,0 +1,8 @@
+import TableCell from '@mui/material/TableCell';
+import { styled } from '@mui/system';
+
+const TableCellCenter = styled(TableCell)({
+  textAlign: 'right',
+});
+
+export default TableCellCenter;
