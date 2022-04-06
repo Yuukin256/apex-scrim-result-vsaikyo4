2962480d498f6b769b6e6998e4404d65b39c1063
Yuukin256 <Yuukin256@gmail.com>
2022-04-06 13:12:15 +0900

    ツールチップの挙動を改善

--- src/components/atoms/Tooltip.tsx
+++ src/components/atoms/Tooltip.tsx
@@ -1,11 +1,19 @@
+import { ClickAwayListener } from '@mui/material';
 import MuiTooltip, { TooltipProps } from '@mui/material/Tooltip';
-import { VFC } from 'react';
+import { useState, VFC } from 'react';
 
 const Tooltip: VFC<TooltipProps> = ({ children, ...props }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <MuiTooltip {...props} enterTouchDelay={0}>
-      {children}
-    </MuiTooltip>
+    <ClickAwayListener onClickAway={handleClose}>
+      <MuiTooltip open={open} onOpen={handleOpen} onClose={handleClose} onTouchEndCapture={handleOpen} {...props}>
+        {children}
+      </MuiTooltip>
+    </ClickAwayListener>
   );
 };
 
