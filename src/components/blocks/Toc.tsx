304a01c7dc012c7a06c09279eae79dce6442a133
Yuukin256 <Yuukin256@gmail.com>
2022-04-10 23:01:25 +0900

    目次をもとに戻す

--- src/components/blocks/Toc.tsx
+++ src/components/blocks/Toc.tsx
@@ -9,9 +9,9 @@ import { VFC } from 'react';
 const Toc: VFC<BoxProps> = (props) => {
   const data = [
     {
-      name: '1日目',
-      url: '/day1',
-      top3: ['？？？', '？？？', '？？？'],
+      name: 'テストデータ',
+      url: '/test',
+      top3: ['胡桃のあチーム', '歌衣メイカチーム', '星川サラチーム'],
     },
   ];
 
