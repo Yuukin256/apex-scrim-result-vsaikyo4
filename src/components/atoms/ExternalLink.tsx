10dffe135fb9427b2b8a357c01f6c7b4caa7fba1
Yuukin256 <Yuukin256@gmail.com>
2022-04-12 19:32:04 +0900

    リンクコンポーネント作成
    949aa3838dff6828bb4af61e399141bc75c2b48a

--- src/components/atoms/ExternalLink.tsx
+++ src/components/atoms/ExternalLink.tsx
@@ -1,9 +1,11 @@
-import Link, { LinkProps } from '@mui/material/Link';
+import LaunchIcon from '@mui/icons-material/Launch';
 import { VFC } from 'react';
+import Link, { LinkProps } from './Link';
 
 const ExternalLink: VFC<LinkProps> = ({ children, ...props }) => (
   <Link target='_blank' rel='noopener' {...props}>
     {children}
+    <LaunchIcon sx={{fontSize: '0.8rem', verticalAlign: 'middle'}} />
   </Link>
 );
 
