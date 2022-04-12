34f1afac3a1e5df20ca50e48cda8906a78b8f7a5
Yuukin256 <Yuukin256@gmail.com>
2022-04-12 17:22:33 +0900

    トップで戻るボタンはスクロールするようにする

--- src/components/atoms/FabToTop.tsx
+++ src/components/atoms/FabToTop.tsx
@@ -1,11 +1,30 @@
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import Fab from '@mui/material/Fab';
-import React from 'react';
+import Zoom from '@mui/material/Zoom';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
 
-const FabToTop: React.VFC = () => (
-  <Fab className='bottom-5 right-5 fixed' color='primary' href='#top'>
-    <ArrowUpwardIcon htmlColor='white' />
-  </Fab>
-);
+interface Props {
+  window?: () => Window;
+}
+
+const FabToTop: React.VFC<Props> = ({ window }) => {
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = () => {
+    scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <Fab className='bottom-5 right-5 fixed' color='primary' onClick={handleClick}>
+        <ArrowUpwardIcon htmlColor='white' />
+      </Fab>
+    </Zoom>
+  );
+};
 
 export default FabToTop;
