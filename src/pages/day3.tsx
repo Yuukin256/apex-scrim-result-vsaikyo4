164ccf0869dce69e73d42d744134f89b3034df1a
Yuukin256 <Yuukin256@gmail.com>
2022-04-13 20:22:06 +0900

    3日目 準備

---
+++ src/pages/day3.tsx
@@ -0,0 +1,32 @@
+import Alert from '@mui/material/Alert';
+import type { NextPage } from 'next';
+import InlineMenuIcon from 'components/atoms/InlineMenuIcon';
+import SiteDescription from 'components/blocks/SiteDescription';
+import StatsView from 'components/blocks/StatsView';
+import Layout from 'components/layouts/Layout';
+import data from 'data/day3.json';
+import { formatData } from 'utils/resultData';
+
+const Page: NextPage = () => {
+  const { team: teamResult, player: playerResult } = formatData(data);
+  return (
+    <Layout title='3日目'>
+      <SiteDescription />
+
+      <Alert severity='info'>
+        このページにはカスタム3日目の結果を表示しています。他の日の試合結果は左上のメニュー
+        <InlineMenuIcon />
+        からご覧いただけます。
+      </Alert>
+
+      <StatsView
+        statsTitle='3日目 (4月13日)'
+        teamResult={teamResult}
+        playerResult={playerResult}
+        defaultNumberOfMatches={5}
+      />
+    </Layout>
+  );
+};
+
+export default Page;
